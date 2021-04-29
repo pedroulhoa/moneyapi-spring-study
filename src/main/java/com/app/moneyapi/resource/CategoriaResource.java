52aea4ea7c244bce0fd3fbfb934a4164e7e076d0
Pedro Ulhoa <pedroulhoa@live.com>
2021-04-29 20:55:56 -0300

    feat: categoria Resource

--- src/main/java/com/app/moneyapi/resource/CategoriaResource.java
+++ src/main/java/com/app/moneyapi/resource/CategoriaResource.java
@@ -3,10 +3,12 @@ package com.app.moneyapi.resource;
 import com.app.moneyapi.entity.Categoria;
 import com.app.moneyapi.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -19,6 +21,24 @@ public class CategoriaResource {
     @GetMapping
     public List<Categoria> getAll() {
         return categoriaRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Categoria> create(@RequestBody Categoria categoria, HttpServletResponse response) {
+        Categoria categoriaSave = categoriaRepository.save(categoria);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+                .buildAndExpand(categoriaSave.getCodigo()).toUri();
+        response.setHeader("Location", uri.toASCIIString());
+
+        return ResponseEntity.created(uri).body(categoriaSave);
+    }
+
+    @GetMapping("/{codigo}")
+    public ResponseEntity getCategoria(@PathVariable Long codigo) {
+        return categoriaRepository.findById(codigo)
+                .map(c -> ResponseEntity.ok().body(c))
+                .orElse(ResponseEntity.notFound().build());
     }
 
 }
